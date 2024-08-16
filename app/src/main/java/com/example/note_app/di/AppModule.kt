8fbb55644eb0971c03c35ef3a25f840d7b88f8ca
scomor55 <scomor2@etf.unsa.ba>
2024-08-16 22:24:33 +0200

    addEditNoteViewModel

--- app/src/main/java/com/example/note_app/di/AppModule.kt
+++ app/src/main/java/com/example/note_app/di/AppModule.kt
@@ -8,6 +8,7 @@ import com.example.note_app.feature_note.data.repository.NoteRepositoryImplement
 import com.example.note_app.feature_note.domain.repository.NoteRepository
 import com.example.note_app.feature_note.domain.use_case.AddNote
 import com.example.note_app.feature_note.domain.use_case.DeleteNote
+import com.example.note_app.feature_note.domain.use_case.GetNote
 import com.example.note_app.feature_note.domain.use_case.GetNotes
 import com.example.note_app.feature_note.domain.use_case.NoteUseCases
 import dagger.Module
@@ -43,7 +44,8 @@ object AppModule {
         return NoteUseCases(
             getNotes = GetNotes(repository),
             deleteNote = DeleteNote(repository),
-            addNote = AddNote(repository)
+            addNote = AddNote(repository),
+            getNote = GetNote(repository)
         )
     }
 }
